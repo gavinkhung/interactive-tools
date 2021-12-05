e6f102b7b36bf7a42c564ff95d33b3f4101805bc
Gavin Hung <gavinkhung@gmail.com>
2021-12-05 00:46:43 -0800

    Started winter package

--- winter/src/App.js
+++ winter/src/App.js
@@ -1,50 +1,94 @@
 import React, { useEffect, useState } from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
-const App = () => {
-  const [seconds, setSeconds] = useState(7);
-  const [isActive, setIsActive] = useState(false);
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
 
   useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        setSeconds((seconds) => seconds - 1);
-      }, 1000);
-    }
-    if (seconds <= 0) {
-      setIsActive(false);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const App = () => {
+  const { width, height } = useWindowSize();
+
+  const [playing, toggle] = useAudio("Intro.mp3");
 
   return (
-    <div className="bg-background h-screen w-screen flex flex-col space-y-4 justify-center items-center ">
-      <div className="bg-white text-black font-bold text-7xl tracking-wider rounded-full h-40 w-40 flex items-center justify-center">
-        {seconds <= 0 ? 0 : seconds}
+    <div className="bg-blue-200 h-screen w-screen flex flex-col justify-around px-24">
+      <Confetti
+        width={width}
+        height={height}
+        colors={["#ffffff"]}
+        drawShape={(ctx) => {
+          ctx.beginPath();
+          for (let i = 0; i < 10; i++) {
+            const angle = 0.35 * i;
+            const x = 1.5 * angle * Math.cos(angle);
+            const y = 1.5 * angle * Math.sin(angle);
+            ctx.lineTo(x, y);
+          }
+          ctx.stroke();
+          ctx.closePath();
+        }}
+        gravity={0.05}
+      />
+
+      <div className="">
+        <h1 className="uppercase text-center text-2xl md:text-6xl text-white font-extrabold">
+          Winter Sports
+        </h1>
       </div>
-      <div className="flex flex-row space-x-6">
+      <div className="grid grid-cols-2 gap-24">
+        <Article url="." />
+        <Article url="." />
+        <Article url="." />
+        <Article url="." />
+      </div>
+      <div className="mx-auto">
         <button
-          className="bg-white text-black hover:text-gray-600 relative rounded-lg shadow-md px-4 py-2.5 cursor-pointer flex focus:outline-none"
-          onClick={() => {
-            setIsActive(!isActive);
-            if (seconds <= 0) {
-              setSeconds(7);
-            }
-          }}
+          onClick={toggle}
+          className="text-center focus:outline-none text-black text-xl py-2 px-3 rounded-md bg-gray-100 hover:bg-gray-200 hover:shadow-lg font-medium"
         >
-          {isActive ? "Stop" : "Start"}
+          {playing ? "Disable Sound" : "Enable Sound"}
         </button>
-        {/* <button
-          className="bg-white text-black hover:text-gray-600 relative rounded-lg shadow-md px-4 py-2.5 cursor-pointer flex focus:outline-none"
-          onClick={() => {
-            setSeconds(7);
-            setIsActive(false);
-          }}
-        >
-          Reset
-        </button> */}
       </div>
     </div>
+  );
+};
+
+const Article = ({ url }) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className="">
+      <div className="p-4 bg-gray-400 hover:bg-gray-400 cursor-pointer backdrop-blur-md filter rounded-3xl shadow">
+        <div className="flex flex-row space-x-2">
+          <div className="">
+            <img
+              className="h-36 rounded-xl object-cover"
+              src="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?auto=format&fit=crop&w=1160&q=80"
+              alt=""
+            />
+          </div>
+          <div className="">
+            <h3 className="text-md font-bold text-gray-100">Headline 1</h3>
+            <p className="mt-0.5 text-sm text-gray-300">Deck 1</p>
+          </div>
+        </div>
+      </div>
+    </a>
   );
 };
 
