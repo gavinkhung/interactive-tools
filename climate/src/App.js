09143b2ea203bab4029d27b90cda3445cfaaf7a4
Gavin Hung <gavinkhung@gmail.com>
2021-12-27 15:07:51 -0800

    Added changes to climate package

--- climate/src/App.js
+++ climate/src/App.js
@@ -66,89 +66,141 @@ function App() {
   }, [zoom]);
 
   return (
-    <div className="w-full min-h-screen flex flex-col bg-blue-100">
-      <div className="flex-none flex flex-row justify-between items-center w-full px-2">
+    <div className="w-full min-h-screen flex flex-row justify-between bg-blue-100 p-2">
+      <div className="flex-none">
         <a
           href="https://elestoque.org/"
           className="text-md text-black underline font-trebuchet"
         >
           Back to El Estoque
         </a>
-        <div className="space-y-1 flex flex-col max-w-5xl">
-          <h1 className="flex-none text-black py-2 text-center uppercase text-2xl md:text-5xl font-extrabold font-oswald">
-            Addressing climate change
-          </h1>
+      </div>
+      <div className="grow space-y-1 flex flex-col">
+        <h1 className="flex-none text-black py-2 text-center uppercase text-2xl md:text-5xl font-extrabold font-oswald">
+          Addressing climate change
+        </h1>
+        <p className="flex-none text-gray-800 text-center text-lg md:text-2xl font-oswald">
+          Examining the approach to climate change on four different levels
+        </p>
+        <p className="flex-none text-gray-700 pb-16 text-center text-lg md:text-2xl font-oswald">
+          By:{" "}
+          <a
+            href="https://elestoque.org/staff_name/michelle-chen/"
+            target="_blank"
+            className="underline"
+          >
+            Michelle Chen
+          </a>
+          ,{" "}
+          <a
+            href="https://elestoque.org/staff_name/melody-cui/"
+            target="_blank"
+            className="underline"
+          >
+            Melody Cui
+          </a>
+          ,{" "}
+          <a
+            href="https://elestoque.org/staff_name/mikaylah-du/"
+            target="_blank"
+            className="underline"
+          >
+            Mikaylah Du
+          </a>
+          ,{" "}
+          <a
+            href="https://elestoque.org/staff_name/jefferson-le/"
+            target="_blank"
+            className="underline"
+          >
+            Jefferson Le
+          </a>
+          ,{" "}
+          <a
+            href="https://elestoque.org/staff_name/lance-tong/"
+            target="_blank"
+            className="underline"
+          >
+            Lance Tong
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://elestoque.org/staff_name/angela-zhang/"
+            target="_blank"
+            className="underline"
+          >
+            Angela Zhang
+          </a>
+        </p>
+        <div className="grow flex flex-col justify-center md:flex-row mx-10 pb-10">
+          <div className="max-w-2xl space-y-5">
+            <div className="space-y-3">
+              <div className="flex flex-col space-y-3">
+                <h2 className="text-3xl text-gray-800 font-bold font-trebuchet overflow-hidden line-clamp-2">
+                  {selectedArticle["headline"]}
+                </h2>
+                <h3 className="text-xl text-gray-600 font-semibold font-trebuchet">
+                  {selectedArticle["deck"]}
+                </h3>
+              </div>
+              <div className="flex justify-center items-center">
+                <a
+                  target="_blank"
+                  href={selectedArticle["url"]}
+                  className="text-gray-800"
+                >
+                  <img
+                    className="w-96"
+                    src={selectedArticle["imageUrl"]}
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row justify-start py-4">
+            <div className="">
+              <input
+                type="range"
+                className="range purple h-full"
+                list="temperature"
+                min="0"
+                max="100"
+                value={zoom}
+                onChange={(e) => setZoom(e.target.value)}
+                step="3"
+              />
+              <datalist id="temperature">
+                <option value="0"></option>
+                <option value="33"></option>
+                <option value="66"></option>
+                <option value="100"></option>
+              </datalist>
+            </div>
+            <div className="flex flex-col justify-between h-full">
+              {articles.map((article, index) => (
+                <div className="px-4 py-3 bg-white hover:bg-gray-100 cursor-pointer backdrop-blur-md filter rounded-xl shadow text-center">
+                  <a
+                    target="_blank"
+                    href={article["url"]}
+                    className="text-gray-800 font-trebuchet"
+                  >
+                    {articles[articles.length - 1 - index]["level"]}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="flex-none">
         <a
           href="https://elestoque.org/"
           className="text-md text-black underline opacity-0"
         >
           Back to El Estoque
         </a>
-      </div>
-      <p className="flex-none text-gray-800 pb-16 text-center text-lg md:text-2xl font-oswald">
-        Examining the approach to climate change on four different levels
-      </p>
-      <div className="grow flex flex-col md:flex-row mx-10 pb-10">
-        <div className="flex-1 space-y-5">
-          <div className="space-y-3">
-            <div className="flex flex-col space-y-3">
-              <h2 className="text-3xl text-gray-800 font-bold font-trebuchet">
-                {selectedArticle["headline"]}
-              </h2>
-              <h3 className="text-xl text-gray-600 font-semibold font-trebuchet">
-                {selectedArticle["deck"]}
-              </h3>
-            </div>
-            <div className="flex justify-center items-center">
-              <a
-                target="_blank"
-                href={selectedArticle["url"]}
-                className="text-gray-800"
-              >
-                <img
-                  className="w-96"
-                  src={selectedArticle["imageUrl"]}
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-row justify-center flex-1 py-4">
-          <div className="">
-            <input
-              type="range"
-              className="range purple h-full"
-              list="temperature"
-              min="0"
-              max="100"
-              value={zoom}
-              onChange={(e) => setZoom(e.target.value)}
-              step="3"
-            />
-            <datalist id="temperature">
-              <option value="0"></option>
-              <option value="33"></option>
-              <option value="66"></option>
-              <option value="100"></option>
-            </datalist>
-          </div>
-          <div className="flex flex-col justify-between h-full">
-            {articles.map((article, index) => (
-              <div className="px-4 py-3 bg-white hover:bg-gray-100 cursor-pointer backdrop-blur-md filter rounded-xl shadow text-center">
-                <a
-                  target="_blank"
-                  href={article["url"]}
-                  className="text-gray-800 font-trebuchet"
-                >
-                  {articles[articles.length - 1 - index]["level"]}
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
       </div>
     </div>
   );
